e5fd88a9c1710402b37f8dc3de83b469a3472e67
miermontoto <UO283319@uniovi.es>
2022-04-21 11:53:03 +0200

    fix Cliente

--- PL/eventos/poe-03/src/cliente/Cliente.java
+++ PL/eventos/poe-03/src/cliente/Cliente.java
@@ -28,8 +28,8 @@ public class Cliente {
 	 * Método que limpia la salida.
 	 */
 	private static void limpiar() {
-		// TODO: fix, obviamente no funciona.
 		System.out.print("\033[H\033[2J");
+		System.out.flush();
 	}
 
 	/**
